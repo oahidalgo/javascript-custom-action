b7814b62c7fe098e8ff0589459b12358fd995750
oahidalgo <ohgovilla@gmail.com>
2024-09-04 07:30:51 -0600

    TEST

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,7 +11,7 @@ function run() {
   // 2) Upload files
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
-
+  console.log(`Files uploaded to ${s3Uri}`);
   const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
   core.setOutput('website-url', websiteUrl); // echo 'website-url=...' >> $GITHUB_OUTPUT
 }
